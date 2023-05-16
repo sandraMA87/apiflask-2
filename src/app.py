5692a1726873c0f558dfb534847be781d2774ca5
Sandra Montero <122152633+sandraMA87@users.noreply.github.com>
2023-05-16 21:34:13 +0000

    apiflask2

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,11 +36,171 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+
+    users = User.query.all()
+    user_list = [element.serialize() for element in users]
+
+
+    return jsonify(user_list), 200
+
+
+
+# GET DE PLANET-GALAXY
+
+@app.route('/planet-galaxy', methods=['GET'])
+def get_relation_planet_galaxy():
+    planets = Planet.query.all()
+    response = []
+    for planet in planets:
+        response.append({
+            'planet': planet.name,
+            'galaxy_id': planet.galaxy_id,
+            'galaxy_name': planet.galaxy.name
+        })
+    return jsonify(response)
+
+
+# GET DE PLANETS
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    allPlanets = Planet.query.all()
+    result = [element.serialize() for element in allPlanets]
+    return jsonify(result), 200
+
+
+# POST DE FAVORITE DE PLANET_ID
+current_logged_user_id = 1
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    
+    user = User.query.get(current_logged_user_id)
+
+    new_favorite = Favorite(user_id=current_logged_user_id, planet_id=planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Nuevo planeta agregado correctamente", 
+        "favorite": new_favorite.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+
+# POST DE PLANETS
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+
+    # obtener los datos de la petición que están en formato JSON a un tipo de datos entendibles por pyton (a un diccionario). En principio, en esta petición, deberían enviarnos 3 campos: el nombre, la descripción del planeta y la población
+    data = request.get_json()
+
+    # creamos un nuevo objeto de tipo Planet
+    planet = Planet(name=data['name'], description=data['description'])
+
+    # añadimos el planeta a la base de datos
+    db.session.add(planet)
+    db.session.commit()
+
+    return jsonify(planet.serialize()), 200
+
+# DELETE DE PLANETS
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    favorite = Favorite.query.filter_by(user_id=current_logged_user_id, planet_id=planet_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        response_body = {
+            "msg": "Planeta favorito eliminado correctamente"
+        }
+        return jsonify(response_body), 200
+   
+
+
+
+# GET DE CHARACTERS
+
+@app.route('/character', methods=['GET'])
+def get_character():
+    allCharacters = Character.query.all()
+    result = [element.serialize() for element in allCharacters]
+    return jsonify(result), 200
+
+
+
+# GET DE CHARACTER_ID
+    
+@app.route('/favorite/character/<int:character_id>', methods=['POST'])
+def add_favorite_character(character_id):
+    
+    user = User.query.get(current_logged_user_id)
+
+    new_favorite = Favorite(user_id=current_logged_user_id, character_id=character_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Nuevo character agregado correctamente", 
+        "favorite": new_favorite.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+
+# POST DE CHARACTERS
+
+@app.route('/character', methods=['POST'])
+def post_character():
+
+    # obtener los datos de la petición que están en formato JSON a un tipo de datos entendibles por pyton (a un diccionario). En principio, en esta petición, deberían enviarnos 3 campos: el nombre, la descripción del planeta y la población
+    data = request.get_json()
+
+    # creamos un nuevo objeto de tipo Character
+    character = Character(name=data['name'], gender=data['gender'])
+
+    # añadimos el character a la base de datos
+    db.session.add(character)
+    db.session.commit()
+
+    return jsonify(character.serialize()), 200
+
+# DELETE DE CHARACTERS
+
+@app.route('/favorite/character/<int:character_id>', methods=['DELETE'])
+def delete_favorite_character(character_id):
+    favorite = Favorite.query.filter_by(user_id=current_logged_user_id, character_id=character_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        response_body = {
+            "msg": "Personaje favorito eliminado correctamente"
+        }
+        return jsonify(response_body), 200
+
+
+# GET DE FAVORITES DE USERS
+
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    
+    user = User.query.get(current_logged_user_id)
+    favorites = user.favorites
+    serialized_favorites = [f.serialize() for f in favorites]
+
+    response_body = {
+        "msg": f"Aqui tienes los favoritos de {user.email}",
+        "favorites": serialized_favorites
     }
 
     return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -6,10 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    favorites = db.relationship('Favorite', backref='user')
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'{self.email}'
 
     def serialize(self):
         return {
@@ -17,3 +19,67 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Galaxy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    coordinate_center_x = db.Column(db.Float, nullable=False) 
+    coordinate_center_y = db.Column(db.Float, nullable=False)        
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(240))
+
+    # OBLIGATORIO para establecer la relación
+    galaxy_id = db.Column(db.Integer, db.ForeignKey('galaxy.id'), nullable=False)
+    galaxy = db.relationship('Galaxy')
+
+    
+    def __repr__(self):
+        return f'{self.name}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+        } 
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(240))
+    
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+        }   
+    
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+
+    planet = db.relationship('Planet', foreign_keys=[planet_id])
+    character = db.relationship('Character', foreign_keys=[character_id])
+
+
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'planet': self.planet.serialize() if self.planet else None,
+            'character': self.character.serialize() if self.character else None,
+        }
+        
